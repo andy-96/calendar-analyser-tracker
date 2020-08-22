2eeb6f4e639e1623c40b853f69b761064c3ab4f3
Andy Chen <mail@andychen.de>
2020-08-22 23:03:44 +0200

    add todo

--- backend/index.js
+++ backend/index.js
@@ -4,6 +4,8 @@ const { googleAuth } = require('./googleService.js')
 async function main() {
   const auth = await googleAuth()
 
+  // TODO: Save calendars and events to MongoDB!! Some kind of sync function
+  // that fetches data from Google and compares it with MongoDB entries.
   const events = await fetchEvents(auth)
   const calendars = await fetchCalendars(auth)
   console.log(calendars.length)
