61e449b9f3d2c17c1bbd7b5d26a3e6d1afcfc9ae
Andy Chen <mail@andychen.de>
2020-08-23 16:34:48 +0200

    add todo

--- backend/googleService.js
+++ backend/googleService.js
@@ -31,35 +31,14 @@ async function fetchEvents () {
   return items
 }
 
-function listEvents () {
-  const calendar = google.calendar({ version: 'v3', auth: googleAuth() })
-  calendar.events.list({
-    calendarId: 'primary',
-    timeMin: (new Date()).toISOString(),
-    maxResults: 10,
-    singleEvents: true,
-    orderBy: 'startTime'
-  }, (err, res) => {
-    if (err) return console.log('The API returned an error: ' + err)
-    const events = res.data.items
-    if (events.length) {
-      console.log('Upcoming 10 events:')
-      events.map((event, i) => {
-        const start = event.start.dateTime || event.start.date
-        console.log(`${start} - ${event.summary}`)
-      })
-    } else {
-      console.log('No upcoming events found.')
-    }
-  })
-}
-
+// TODO: saveEvents to mongoDB
 
 function test() {
   return new Promise((resolve, reject) => {
     setTimeout(() => resolve('whaat'), 1000)
   })
 }
+
 ;(async () => {
   const sth = await saveCalendars()
   console.log(sth)
